fc6b868e8b8ae6f13bb265c91a5d9d1e0ebc8caa
MD SOHAIL ALI <90498286+sohaill123@users.noreply.github.com>
2023-06-13 21:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digits) {
   //Complete the function
+	const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  // Array to store the resulting combinations
+  const combinations = [];
+
+  // Recursive helper function to generate combinations
+  function backtrack(combination, nextDigits) {
+    // Base case: no more digits to process
+    if (nextDigits.length === 0) {
+      combinations.push(combination);
+      return;
+    }
+
+    // Get the letters corresponding to the current digit
+    const currentDigit = nextDigits[0];
+    const letters = digitToLetters[currentDigit];
+
+    // Recursive call for each letter
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(combination + letter, nextDigits.slice(1));
+    }
+  }
+
+  // Call the helper function to generate combinations
+  if (digits.length !== 0) {
+    backtrack('', digits);
+  }
+
+  // Sort the resulting combinations lexicographically
+  combinations.sort();
+
+  return combinations;
 }
 
 module.exports = letterCombinations;
